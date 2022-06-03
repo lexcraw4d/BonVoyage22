77b07c57603bab951af7233df7effa4b40dbda10
Sylvain Leroux <sylvain@chicoree.fr>
2022-06-04 01:05:53 +0200

    Send a hard-coded request to the Place API

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   port: process.env.BV2_PORT || 3000,
   api: {
-    key: process.env.B2_API_KEY,
+    key: process.env.BV2_API_KEY,
   }
 }
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const config = require('../config.js');
+
+const { Client, PlaceInputType, PlaceType1, PlaceType2 } = require("@googlemaps/google-maps-services-js");
+const client = new Client({});
 
 router.get('/test', function(req, res, next) {
   res.json({message:'OK'});
+});
+
+router.get('/loc', async function(req, res, next) {
+  const params = {
+    input: "Museum of Modern Art",
+    inputtype: PlaceInputType.textQuery,
+    key: config.api.key,
+    fields: ["place_id", "name", "formatted_address"],
+  };
+
+  const r = await client.findPlaceFromText({ params: params });
+  res.json(r.data);
 });
 
 module.exports = router;
