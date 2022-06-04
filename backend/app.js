5b7890be23f8d8de7e13859b906b3ae8e26fa695
Sylvain Leroux <sylvain@chicoree.fr>
2022-06-04 11:34:53 +0200

    Add route to serve static files from client/
    Issue #5

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,14 @@
 const express = require('express')
 
 const config = require('./config.js');
-const api = require('./routes/api.js');
+const route_api = require('./routes/api.js');
+const route_client = require('./routes/client.js');
 
 const app = express();
 const port = config.port;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.use('/api', api);
+app.use(route_client);
+app.use('/api', route_api);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
